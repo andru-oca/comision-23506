938f653f778e4156adbf783ec99751e0305a0407
anderson ocana <aocanatorres@buenosaires.gob.ar>
2023-09-19 22:00:38 -0300

    primer clase de javascript :)

--- 13/asset/js/index.js
+++ 13/asset/js/index.js
@@ -5,3 +5,36 @@ console.table(
         email:"mail@mail.com"
     }
 )
+
+
+
+let userName = prompt("Señor usuario, ingrese su nombre")
+
+let mensajeDeBienvenida = "Hola!!" + userName
+
+document.write(
+    "<h1>"+
+    mensajeDeBienvenida
+    +"</h1>"
+);
+
+
+let edadUsuario =  prompt("Señor usuario, ingrese su edad")
+
+edadUsuario = Number(edadUsuario) + 10
+
+mensajeDeBienvenida = "<hr> mi edad es " + edadUsuario
+  
+
+
+document.write(
+    "<h1>"+
+    mensajeDeBienvenida
+    +"</h1>"
+);
+
+
+
+
+
+
